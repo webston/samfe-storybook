0021694ead032e7c669a22f994e4e5cfd60e8734
Harry Chakryan <harry@webnerds.se>
2020-09-08 09:06:31 +0300

    Added storybook

---
+++ precache-manifest.53a0d1d7b207ad641a5a7193fdb273a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "url": "vendors~main.dbf8ebdfed1c2a5fc328.bundle.js.LICENSE.txt"
+  },
+  {
+    "url": "vendors~main.dbf8ebdfed1c2a5fc328.bundle.js"
+  },
+  {
+    "revision": "3622cc9ffdb9fb6431f5",
+    "url": "static/css/main.7c230d06.chunk.css"
+  },
+  {
+    "url": "runtime~main.dbf8ebdfed1c2a5fc328.bundle.js"
+  },
+  {
+    "url": "main.dbf8ebdfed1c2a5fc328.bundle.js"
+  },
+  {
+    "revision": "a2d383759f83db44ddd601b9bd5fd952",
+    "url": "iframe.html"
+  }
+];
